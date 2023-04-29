4bf97452c6a9373db421ffc8146be21fb6b0b2b8
YvanPigalle <vanquishracer@gmail.com>
2023-04-29 23:42:03 +0300

    almost done

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@
 
 
 
+
+/*
  void Digits (int num)
 {
  int seconddgt = num / 10%10;
@@ -28,4 +30,67 @@ if (user_num > 99 && user_num < 1000 || user_num < -99 && user_num > -1000)
 {
 Digits (user_num);
 }
+*/
 
+
+
+
+
+//Задача: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//что третьей цифры нет.
+
+
+
+
+
+
+
+void Digits3 (int num)
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+ 
+ int thirddgt = num % 10;
+ {
+ Console.WriteLine ($"Third digit is " +thirddgt);
+ }
+ while (num < -999)
+    {
+        num /= 10;
+    }
+     int minusthird = num % 10*-1;
+        if (num < -999)
+ {
+ Console.WriteLine ($"Third digit is " +minusthird);
+ }
+}
+ void Digits1 (int num)
+{
+ int thirddgt = num % 10;
+ int minusthird = num % 10*-1;
+ if (num > 99 && num < 1000)
+ {
+     Console.WriteLine ($"Third digit is " +thirddgt);
+ }
+if (num < -99 && num > -1000)
+{
+ Console.WriteLine ($"Third digit is " +minusthird);
+}
+}
+
+Console.WriteLine("Input your number: ");
+int user_num = Convert.ToInt32(Console.ReadLine());
+if (user_num > -100 && user_num < 100)
+{
+   Console.WriteLine("There is no third digit!"); 
+}
+if (user_num > 99 && user_num < 1000 || user_num < -99 && user_num > -1000)
+{
+Digits1 (user_num);
+}
+if (user_num > 999 || user_num < -999)
+{
+    Digits3 (user_num);
+}
