7023529669d951553822d73854101bd11ea724d3
YvanPigalle <vanquishracer@gmail.com>
2023-05-01 08:28:28 +0300

    done

--- Program.cs
+++ Program.cs
@@ -44,7 +44,7 @@ Digits (user_num);
 
 
 
-
+/*
 void Digits3 (int num)
 {
     while (num > 999)
@@ -74,7 +74,7 @@ void Digits3 (int num)
  {
      Console.WriteLine ($"Third digit is " +thirddgt);
  }
-if (num < -99 && num > -1000)
+ else if (num < -99 && num > -1000)
 {
  Console.WriteLine ($"Third digit is " +minusthird);
 }
@@ -90,7 +90,31 @@ if (user_num > 99 && user_num < 1000 || user_num < -99 && user_num > -1000)
 {
 Digits1 (user_num);
 }
-if (user_num > 999 || user_num < -999)
+else if (user_num > 999 || user_num < -999)
 {
     Digits3 (user_num);
 }
+*/
+
+
+
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+ //и проверяет, является ли этот день выходным.
+
+
+/*
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("It's day off!))");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Bro...no");
+  }
+  else Console.WriteLine("It's weekday((");
+}
+
+
+Console.Write("Insert number of week day: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+CheckingTheDayOfTheWeek(dayNumber);
+*/
